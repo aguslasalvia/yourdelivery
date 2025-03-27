777f2a5b877e15177fa3ffa8e1527ba4784adc50
martinleib <m@martinleib.uy>
2025-03-26 21:09:26 -0300

    Add: .gitignore

--- Core/Entities/Agency.cs
+++ Core/Entities/Agency.cs
@@ -1,10 +1,10 @@
 ﻿namespace Core.Entities
 {
     
-    public class Agency(String name, String direction,float latitude, float longitude)
+    public class Agency(String name, String address,float latitude, float longitude)
     {
         public String Name { get; set; } = name;
-        public String Direction { get; set; } = direction;
+        public String Direction { get; set; } = address;
         public float Latitude { get; set; } = latitude;
         public float Longitude { get; set; } = longitude;
 
--- Core/Entities/UrgentShipping.cs
+++ Core/Entities/UrgentShipping.cs
@@ -3,9 +3,9 @@
 namespace Core.Entities
 {
     public class UrgentShipping(int id, int tracking, decimal weight, User employee, User client, 
-        ShippingState state,String direction,DateTime send,DateTime? arrival ) : Shipping(id, tracking, weight, employee, client, state)
+        ShippingState state,String address,DateTime send,DateTime? arrival ) : Shipping(id, tracking, weight, employee, client, state)
     {
-        public String Direction { get; set; } = direction;
+        public String Address { get; set; } = address;
         public DateTime Send { get; set; } = send;
         public DateTime? Arrival { get; set; } = arrival;
 
