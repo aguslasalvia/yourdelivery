64fd422876a378b35231e9a7f879871857379cda
AgusLasalvia <agus.blumenfeld13@gmail.com>
2025-03-30 16:37:36 -0300

    UI: Tracking View + Users list icons change

---
+++ Presentation/Controllers/ShippingController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+
+namespace Presentation.Controllers
+{
+    public class ShippingController() : Controller
+    {
+        [HttpGet]
+        public IActionResult Tracking(int? trackingNumber)
+        {
+            ViewData["Title"] = "Tracking";
+            ViewBag.Shipping = trackingNumber;
+            ViewBag.State = null;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
