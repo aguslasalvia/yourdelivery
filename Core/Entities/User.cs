e2a01d8914a07a878e6b26f122e4d50e84ec686e
AgusLasalvia <agus.blumenfeld13@gmail.com>
2025-04-01 13:09:46 -0300

    Add: ID to all enities

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -1,23 +1,29 @@
 ﻿using Core.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class User ( string name, string lastname, string phone, DateOnly birth,string email, string password, Role role)
+    public class User
     {
-        public Role Role { get; set; } = role;
-        public string Name { get; set; } = name;
-        public string Phone { get; set; } = phone;
-        public string Email { get; set; } = email;
-        public DateOnly Birth { get; set; } = birth;
-        public string Password { get; set; } = password;
-        public string Lastname { get; set; } = lastname;
-        
-        
+        public int Id { get; private set; } 
+        public string Name { get; private set; }
+        public string Lastname { get; private set; }
+        public string Phone { get; private set; }
+        public string Email { get; private set; }
+        public DateOnly Birth { get; private set; }
+        public string Password { get; private set; }
+        public Role Role { get; private set; }
 
+        protected User() { } 
+
+        public User(string name, string lastname, string phone, DateOnly birth, string email, string password, Role role)
+        {
+            Name = name;
+            Lastname = lastname;
+            Phone = phone;
+            Email = email;
+            Birth = birth;
+            Password = password;
+            Role = role;
+        }
     }
 }
