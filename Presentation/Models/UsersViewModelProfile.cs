15aa565affb0501555f53dd3345320d6c914803b
martinleib <m@martinleib.uy>
2025-04-09 16:51:56 -0300

    Add: Models for User view

---
+++ Presentation/Models/UsersViewModelProfile.cs
@@ -0,0 +1,9 @@
+namespace Presentation.Models;
+
+public class UsersViewModelProfile
+{
+    // TODO: acá adentro irían los datos que
+    // TODO: estabamos mandando por Viewbag a esa vista
+    // TODO: lo voy a hacer yo. Si no te convence el nombre
+    // TODO: decime y lo cambio
+}
